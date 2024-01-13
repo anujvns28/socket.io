5644b825dfbffc034026cd93d7d387e8cf41aa98
anujYadav <anujvns28@gmail.com>
2024-01-13 22:26:50 +0530

    today practice socket

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const express = require("express");
 const app = express();
+const {createServer}  = require("node:http")
 
-app.listen("3000",() => {
-    console.log("server stardted ")
+const { Server } = require('socket.io');
+
+const server = createServer(app)
+
+
+
+
+server.listen(4000,() => {
+    console.log("server stardted ",);
 })
+
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
+
+ global.onlineUser = new Map()
+
+ 
+
+
+  io.on('connection', (socket) => {
+   
+    socket.on('chat message', (msg) => {
+        console.log(msg)
+      io.emit('chat message', msg);
+    });
+  });
+
+
+  
+  
